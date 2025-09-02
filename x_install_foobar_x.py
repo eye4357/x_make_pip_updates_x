915cf86affd289ad2a2613989b11cb6dd1b97672
Roy GM <eye4357@outlook.com>
2025-09-01 21:37:09 -0400

    x

--- x_install_foobar_x.py
+++ x_install_foobar_x.py
@@ -1,9 +1,4 @@
-from x_cls_make_pip_x import x_cls_make_pip_x
 
 if __name__ == "__main__":
     # Minimal example: install default packages
-    x_cls_make_pip_x().batch_install([
-        "x_make_markdown_x",
-        "x_make_pypi_x",
-        "x_make_github_clones_x"
-    ])
+
